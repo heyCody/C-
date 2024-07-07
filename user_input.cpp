4f881c1787bfadd62e68528240d057903a0e570a
heyCody <idumukherjee@gmail.com>
2024-07-07 22:25:52 +0530

    thirdcommit

---
+++ user_input.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	/*
+	int n;
+	cin>>n;
+	cout<< "value of n: " << n <<endl;
+	*/
+	int a;
+	a=cin.get(); //ascii value of cin input
+	cout<< a <<endl;
+}
